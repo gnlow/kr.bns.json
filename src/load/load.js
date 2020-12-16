3485a7faee85ef1e603190d3934c0e43e02d05d0
Gnlow <entrydark@gmail.com>
2020-12-16 16:36:09 +0900

    loader

---
+++ src/load/load.js
@@ -0,0 +1,32 @@
+const lines = require("./lines.js")
+console.log(lines)
+
+const pptr = require("puppeteer")
+const { writeFile } = require("fs").promises;
+
+(async () => {
+    const browser = await pptr.launch()
+    const page = await browser.newPage()
+
+    let allStations = []
+
+    for (const [name, id, start, end] of lines) {
+        await page.goto(`https://rail.blue/railroad/logis/line.aspx?id=${id}`, { waitUntil: "networkidle2" })
+        const stations = await page.evaluate(
+            () => _map_station.filter(
+                ([sId, sName, type, lat, long]) => {
+                    return ![
+                        "rc",
+                        "junction",
+                        "base",
+                    ].includes(type)
+                }
+            )
+        )
+        allStations.push(...stations)
+    }
+
+    await browser.close()
+
+    await writeFile("./data.json", JSON.stringify(allStations))
+})();
