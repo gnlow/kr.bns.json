73a4da457ce248a3d11eba3c8039149a6d937da4
Gnlow <entrydark@gmail.com>
2020-12-16 21:11:32 +0900

    일부역 위치 오류 수정

--- src/edit/edit.js
+++ src/edit/edit.js
@@ -3,13 +3,21 @@ const data = JSON.parse(await Deno.readTextFile("../load/data.json"))
 let edited
 
 edited = data.map(([lName, stations]) => {
+    stations = [
+        ...stations.filter(x => x[5] == 65).reverse(),
+        ...stations.filter(x => x[5] == 0),
+        ...stations.filter(x => x[5] == 64),
+    ]
+    if (
+        [
+            "우이",
+        ].includes(lName)
+    ) {
+        stations = stations.sort((a, b) => a[0] - b[0])
+    }
     return [
         lName,
-        [
-            ...stations.filter(x => x[5] == 65).reverse(),
-            ...stations.filter(x => x[5] == 0),
-            ...stations.filter(x => x[5] == 64),
-        ]
+        stations,
     ]
 })
 
